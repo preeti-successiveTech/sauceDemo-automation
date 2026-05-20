0e58867e724dd46dfe45839f6630dec002dbbd4a
Preeti Garg <preeti.garg@preetivrs-36.successivetech.com>
2026-05-20 11:47:46 +0530

    inventory automation script completed

--- sauce-demo-playwright/pages/InventoryPage.ts
+++ sauce-demo-playwright/pages/InventoryPage.ts
@@ -1,46 +1,75 @@
 import { Page, Locator, expect } from "@playwright/test";
-export class InventoryPage{
-    readonly page : Page;
-    readonly inventrylist :Locator;
-    readonly inventryItem : Locator;
-    readonly cartitems: Locator;
-    readonly productNames : Locator;
-    readonly productPrices : Locator;
-    readonly addToCartButton : Locator;
-      constructor(page: Page) {
+export class InventoryPage {
+  readonly page: Page;
+  readonly inventrylist: Locator;
+  readonly inventryItem: Locator;
+  readonly cartitems: Locator;
+  readonly productNames: Locator;
+  readonly productPrices: Locator;
+  readonly firstaddToCartButton: Locator;
+  readonly addToCartButtons: Locator;
+  readonly cartBadge: Locator;
+  readonly removeButton: Locator;
+  readonly sortDropdown: Locator;
+  constructor(page: Page) {
     this.page = page;
     this.inventrylist = page.locator('div[data-test="inventory-list"]');
     this.inventryItem = page.locator('div[data-test="inventory-item"]');
     this.productNames = page.locator('div[data-test="inventory-item-name"]');
     this.productPrices = page.locator('div[data-test="inventory-item-price"]');
-        this.addToCartButton = page.locator('#add-to-cart-sauce-labs-backpack');
+    this.sortDropdown = page.locator(
+      'select[data-test="product-sort-container"]',
+    );
+    this.firstaddToCartButton = page.locator(
+      "#add-to-cart-sauce-labs-backpack",
+    );
+    this.cartBadge = page.locator('span[data-test="shopping-cart-badge"]');
+    this.addToCartButtons = page.locator('[data-test^="add-to-cart"]');
+    this.removeButton = page.locator('[data-test^="remove"]');
+  }
+  async VerifyProductlistVisible() {
+    await expect(this.inventrylist).toBeVisible();
+  }
+  async countProducts() {
+    let count = await this.inventryItem.count();
+    return count;
+  }
+  async verifyProductsContainNameAndPrice() {
+    const nameCount = await this.productNames.count();
 
-    }
-    async VerifyProductlistVisible()
-    {
-        await expect(this.inventrylist).toBeVisible();
-    
-    }
-    async countProducts()
-    {
-        let count = await this.inventryItem.count();
-        return count;
-    }
-    async verifyProductsContainNameAndPrice() {
-
-    const nameCount =
-      await this.productNames.count();
-
-    const priceCount =
-      await this.productPrices.count();
+    const priceCount = await this.productPrices.count();
 
     expect(nameCount).toBeGreaterThan(0);
 
     expect(priceCount).toBeGreaterThan(0);
   }
-  async addFirstProduct()
-  {
-    await this.addToCartButton.nth(0).click();
-  
+  async addFirstProduct() {
+    await this.firstaddToCartButton.click();
+  }
+  async verifyCartProductCount(number: string) {
+    if (number === "0") {
+      await expect(this.cartBadge).toBeHidden();
+      return;
+    }
+    await expect(this.cartBadge).toBeVisible();
+    await expect(await this.cartBadge.textContent()).toBe(number);
+  }
+  async addMultipleProducts(n: number) {
+    for (let i = 0; i < n; i++) {
+      await this.addToCartButtons.nth(0).click();
+    }
+  }
+  async removeItem() {
+    await this.removeButton.click();
+  }
+  async sortProduct(value: string) {
+    await this.sortDropdown.selectOption(value);
+  }
+  async getPrductNames() {
+    return await this.productNames.allTextContents();
+  }
+  async getPrductPrices() {
+    const prices = await this.productPrices.allTextContents();
+    return await prices.map((prices) => Number(prices.replace("$", "")));
   }
 }
--- sauce-demo-playwright/tests/inventory.spec.ts
+++ sauce-demo-playwright/tests/inventory.spec.ts
@@ -1,25 +1,60 @@
-import {test, expect} from "@playwright/test";
+import { test, expect } from "@playwright/test";
 import { LoginPage } from "../pages/LoginPage";
 import { InventoryPage } from "../pages/InventoryPage";
 import { loginData } from "../Test-data/login";
-test.describe("Inventory Automation",()=>{
-    let inventoryPage;
-    let login;
-    test.beforeEach(async({page})=>{
-        login = new LoginPage(page);
-        await login.gotoLoginPage();
-          await login.login(loginData.username, loginData.password);
-        inventoryPage = new InventoryPage(page);
-    });
-    test("Verify Products Are Visible",async({page})=>{
-        await expect(page).toHaveURL("/inventory.html");
-        await inventoryPage.VerifyProductlistVisible();
-        await expect(await inventoryPage.countProducts()).toBeGreaterThan(0);
-    });
-    test("Verify Product Name and Price",async()=>{
-        await inventoryPage.verifyProductsContainNameAndPrice();
-    });
-    test("Add Single Product to Cart",async()=>{
-        await inventoryPage.addFirstProduct();
-    });     
-})
+test.describe("Inventory Automation", () => {
+  let inventoryPage;
+  let login;
+  test.beforeEach(async ({ page }) => {
+    login = new LoginPage(page);
+    await login.gotoLoginPage();
+    await login.login(loginData.username, loginData.password);
+    inventoryPage = new InventoryPage(page);
+  });
+  test("Verify Products Are Visible", async ({ page }) => {
+    await expect(page).toHaveURL("/inventory.html");
+    await inventoryPage.VerifyProductlistVisible();
+    await expect(await inventoryPage.countProducts()).toBeGreaterThan(0);
+  });
+  test("Verify Product Name and Price", async () => {
+    await inventoryPage.verifyProductsContainNameAndPrice();
+  });
+  test("Add Single Product to Cart", async () => {
+    await inventoryPage.addFirstProduct();
+    await inventoryPage.verifyCartProductCount("1");
+  });
+  test("Add Multiple Products", async () => {
+    await inventoryPage.addMultipleProducts(5);
+    await inventoryPage.verifyCartProductCount("5");
+  });
+  test("Remove Product from Inventory", async () => {
+    await inventoryPage.addFirstProduct();
+    await inventoryPage.verifyCartProductCount("1");
+    await inventoryPage.removeItem();
+    await inventoryPage.verifyCartProductCount("0");
+  });
+  test("Verify Sorting A → Z", async () => {
+    await inventoryPage.sortProduct("az");
+    const actualNames = await inventoryPage.getPrductNames();
+    const expectedNames = [...actualNames].sort();
+    expect(actualNames).toEqual(expectedNames);
+  });
+  test("Verify Sorting Z → A", async () => {
+    await inventoryPage.sortProduct("za");
+    const actualNames = await inventoryPage.getPrductNames();
+    const expectedNames = [...actualNames].sort().reverse();
+    expect(actualNames).toEqual(expectedNames);
+});
+test("Verify Price Low → High",async()=>{
+    await inventoryPage.sortProduct("lohi");
+    const actualPrices = await inventoryPage.getPrductPrices();
+    const expectedPrices = [...actualPrices] .sort((a, b) => a - b);
+    expect(actualPrices).toEqual(expectedPrices);
+});
+test("Verify Price High → Low",async()=>{
+    await inventoryPage.sortProduct("hilo");
+    const actualPrices = await inventoryPage.getPrductPrices();
+    const expectedPrices = [...actualPrices].sort((a, b) => b - a);
+    expect(actualPrices).toEqual(expectedPrices);
+});
+});
